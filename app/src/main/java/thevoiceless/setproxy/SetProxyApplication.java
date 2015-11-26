a352a0b110134fe0c615a13784541c8d701ecd96
Riley Moses <victi.resurgo@gmail.com>
2015-11-26 16:39:40 -0700

    Add Butterknife and Timber

---
+++ app/src/main/java/thevoiceless/setproxy/SetProxyApplication.java
@@ -0,0 +1,20 @@
+package thevoiceless.setproxy;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+/**
+ * Created by riley on 11/26/15.
+ */
+public class SetProxyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
--- app/src/main/java/thevoiceless/setproxy/activities/MainActivity.java
+++ app/src/main/java/thevoiceless/setproxy/activities/MainActivity.java
@@ -7,25 +7,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import thevoiceless.setproxy.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.fab) FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
     }
 
+    @OnClick(R.id.fab)
+    public void onFabClick(View view) {
+        Snackbar.make(view, "TODO", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show();
+    }
 }
