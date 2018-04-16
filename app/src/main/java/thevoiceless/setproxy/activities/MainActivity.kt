75af59895a9e2f16467e2812fdacd652aa467252
Riley Moses <victi.resurgo@gmail.com>
2018-04-15 18:13:17 -0600

    Use Kotlin synthetic accessors instead of Butterknife in MainActivity

--- app/src/main/java/thevoiceless/setproxy/activities/MainActivity.kt
+++ app/src/main/java/thevoiceless/setproxy/activities/MainActivity.kt
@@ -5,45 +5,17 @@ import android.graphics.Typeface
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.TextInputLayout
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.text.Editable
 import android.text.TextUtils
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-
-import butterknife.BindView
-import butterknife.ButterKnife
 import butterknife.OnClick
-import io.realm.Realm
-import io.realm.exceptions.RealmPrimaryKeyConstraintException
+import kotlinx.android.synthetic.main.activity_main.*
 import thevoiceless.setproxy.R
 import thevoiceless.setproxy.Utils
 import thevoiceless.setproxy.adapters.ProxyConfigurationAdapter
 import thevoiceless.setproxy.data.ProxyConfiguration
-import thevoiceless.setproxy.views.ProxyListContainer
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
-    @BindView(R.id.toolbar)
-    lateinit var mToolbar: Toolbar
-    @BindView(R.id.host_input_layout)
-    lateinit var mHostInputLayout: TextInputLayout
-    @BindView(R.id.host_input)
-    lateinit var mHostInput: EditText
-    @BindView(R.id.port_input_layout)
-    lateinit var mPortInputLayout: TextInputLayout
-    @BindView(R.id.port_input)
-    lateinit var mPortInput: EditText
-    @BindView(R.id.button_set)
-    lateinit var mSetButton: TextView
-    @BindView(R.id.button_clear)
-    lateinit var mClearButton: TextView
-    @BindView(R.id.proxy_list_container)
-    lateinit var mProxiesList: ProxyListContainer
 
     //    private Realm mRealm;
     private var mCurrentProxy: ProxyConfiguration? = null
@@ -64,8 +36,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
-        setSupportActionBar(mToolbar)
+        setSupportActionBar(toolbar)
 
         //        mRealm = Realm.getInstance(this);
         mWifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
@@ -110,7 +81,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        mProxiesList!!.setOnItemClickListener(ProxyConfigurationAdapter.OnItemClickListener { v, proxy ->
+        proxy_list_container.setOnItemClickListener(ProxyConfigurationAdapter.OnItemClickListener { v, proxy ->
             if (mCurrentProxy != null && mCurrentProxy!!.id == proxy.id) return@OnItemClickListener
             if (Utils.setWifiProxySettings(this@MainActivity, proxy.host, Integer.valueOf(proxy.port))) {
                 mCurrentProxy = proxy
@@ -122,13 +93,13 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        mHostInput!!.addTextChangedListener(object : Utils.TextWatcher() {
+        host_input.addTextChangedListener(object : Utils.TextWatcher() {
             override fun afterTextChanged(s: Editable) {
                 updateButtonState()
             }
         })
 
-        mPortInput!!.addTextChangedListener(object : Utils.TextWatcher() {
+        port_input.addTextChangedListener(object : Utils.TextWatcher() {
             override fun afterTextChanged(s: Editable) {
                 updateButtonState()
             }
@@ -205,63 +176,63 @@ class MainActivity : AppCompatActivity() {
 
     private fun validateHost(): Boolean {
         if (TextUtils.isEmpty(hostString())) {
-            mHostInputLayout!!.error = getString(R.string.host_error_empty)
+            host_input_layout.error = getString(R.string.host_error_empty)
             return false
         }
 
-        mHostInputLayout!!.error = null
+        host_input_layout.error = null
         return true
     }
 
     private fun validatePort(): Boolean {
         if (TextUtils.isEmpty(portString())) {
-            mPortInputLayout!!.error = getString(R.string.port_error_empty)
+            port_input_layout.error = getString(R.string.port_error_empty)
             return false
         }
         if (!Utils.canParseInteger(portString())) {
-            mPortInputLayout!!.error = getString(R.string.port_error_number)
+            port_input_layout.error = getString(R.string.port_error_number)
             return false
         }
 
-        mPortInputLayout!!.error = null
+        port_input_layout.error = null
         return true
     }
 
     private fun hostString(): String {
-        return mHostInput!!.text.toString().trim { it <= ' ' }
+        return host_input.text.toString().trim { it <= ' ' }
     }
 
     private fun portString(): String {
-        return mPortInput!!.text.toString().trim { it <= ' ' }
+        return port_input.text.toString().trim { it <= ' ' }
     }
 
     private fun populateFields(proxy: ProxyConfiguration?) {
         if (proxy != null) {
-            mHostInput!!.setText(proxy.host)
-            mPortInput!!.setText(proxy.port)
+            host_input.setText(proxy.host)
+            port_input.setText(proxy.port)
         } else {
-            mHostInput!!.text = null
-            mPortInput!!.text = null
+            host_input.text = null
+            port_input.text = null
         }
     }
 
     private fun enableSetButton() {
-        mSetButton!!.typeface = mEnabledTypeface
-        mSetButton!!.isEnabled = true
+        button_set.typeface = mEnabledTypeface
+        button_set.isEnabled = true
     }
 
     private fun disableSetButton() {
-        mSetButton!!.typeface = mDisabledTypeface
-        mSetButton!!.isEnabled = false
+        button_set.typeface = mDisabledTypeface
+        button_set.isEnabled = false
     }
 
     private fun enableClearButton() {
-        mClearButton!!.typeface = mEnabledTypeface
-        mClearButton!!.isEnabled = true
+        button_clear.typeface = mEnabledTypeface
+        button_clear.isEnabled = true
     }
 
     private fun disableClearButton() {
-        mClearButton!!.typeface = mDisabledTypeface
-        mClearButton!!.isEnabled = false
+        button_clear.typeface = mDisabledTypeface
+        button_clear.isEnabled = false
     }
 }
